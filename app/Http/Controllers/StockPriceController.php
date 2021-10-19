10d32a9cc48b7cc9164b91c8170418eb3fc29f71
Karthik Nataraj <karthikzmsg@gmail.com>
2021-10-19 18:04:17 +0530

    Added logic to save the API response on each time user get the price

--- app/Http/Controllers/StockPriceController.php
+++ app/Http/Controllers/StockPriceController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use App\Models\Price;
 
 class StockPriceController extends Controller
 {
+    const API_ERROR = "An error occured while processing the request";
+
     public function check(Request $request)
     {
         $symbol= $request->symbol;
@@ -14,8 +17,25 @@ class StockPriceController extends Controller
         $response = Http::get($apiUrl);
         $stockPrice  = $response->json();
 
-        return response()->json(array(
-            'price'=> $stockPrice["Global Quote"]["05. price"]
-        ), 200);
+        if($response->status() === 200 && isset($stockPrice["Global Quote"]) && count($stockPrice["Global Quote"]) > 0){
+            
+            $priceDetails = [
+                "symbol" => $stockPrice["Global Quote"]["01. symbol"],
+                "open" => $stockPrice["Global Quote"]["02. open"],
+                "high" => $stockPrice["Global Quote"]["03. high"],
+                "low" => $stockPrice["Global Quote"]["04. low"],
+                "price" => $stockPrice["Global Quote"]["05. price"],
+                "volume" => $stockPrice["Global Quote"]["06. volume"],
+                "latest_trading_day" => $stockPrice["Global Quote"]["07. latest trading day"],
+                "previous_close" => $stockPrice["Global Quote"]["08. previous close"],
+                "change" => $stockPrice["Global Quote"]["09. change"],
+            ];
+
+            Price::create($priceDetails);
+
+            return response()->json($priceDetails, $response->status());
+        } else {
+            return response()->json(self::API_ERROR, $response->status());
+        }
     }
 }
