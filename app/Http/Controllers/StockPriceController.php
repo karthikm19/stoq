0c4339e139c4bccf2143af4a5e600b6392c732fe
Karthik Nataraj <karthikzmsg@gmail.com>
2021-10-19 17:09:19 +0530

    Using Laravel's HTTP response method instead of custom logic to get jSON

--- app/Http/Controllers/StockPriceController.php
+++ app/Http/Controllers/StockPriceController.php
@@ -12,8 +12,10 @@ class StockPriceController extends Controller
         $symbol= $request->symbol;
         $apiUrl = sprintf(env("API_URL"), env("API_FUNCTION"), $symbol, env("API_KEY"));
         $response = Http::get($apiUrl);
-        $stockPrice  = json_decode($response->body());
+        $stockPrice  = $response->json();
 
-        return response()->json(array('price'=> $stockPrice->{"Global Quote"}->{"05. price"}), 200);
+        return response()->json(array(
+            'price'=> $stockPrice["Global Quote"]["05. price"]
+        ), 200);
     }
 }
